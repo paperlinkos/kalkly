0d2c17a5e65ec3bb5e9630409b4a369e1ff0f89f
Christ Embassy Abuja Zone1 <christembassyabujazone1@Mac.lan>
2026-09-08 09:30:59 +0100

    feat: Add native Android and iOS mobile app projects (Capacitor)

---
+++ android/app/src/main/java/com/dynamo/kalkly/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dynamo.kalkly;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dynamo.kalkly',
+  appName: 'DYNAMO SYSTEM',
+  webDir: 'dist'
+};
+
+export default config;
